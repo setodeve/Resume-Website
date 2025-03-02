8d68f71ab1f87f5c3288776484d67a2c907a3d44
setodeve <keinick11@outlook.com>
2025-03-02 17:25:05 +0900

    fix: simplify cat emojis and update header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
-  const cats = ['🐱', '🐈', '😺', '😻', '😹', '😽', '🙀', '😿', '😾', '😼', '🐾', '🐈‍⬛'];
-  const [cat, setCat] = useState('🐱');
+  const cats = ['🐱','😺', '🙀', '😼'];
+  const [cat, setCat] = useState(cats[0]);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * cats.length);
@@ -14,7 +14,7 @@ export default function Header() {
   return (
     <header className="inset-x-0 bg-neutral-300 dark:bg-neutral-900 sm:relative">
       <div className="mx-auto justify-between p-3 sm:flex sm:justify-between sm:max-w-4xl sm:p-4">
-        <div className="text-2xl">{cat}</div>
+        <div className="text-2xl">Resume {cat}</div>
         <div className="flex items-center font-bold justify-end">
           <Link className="mx-2" href="https://github.com/setodeve" target="_blank" aria-label="GitHub">Github</Link>
           <Link className="mx-2" href="https://qiita.com/keiswe" target="_blank" aria-label="Qiita">Qiita</Link>
