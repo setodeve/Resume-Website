735374c6bc3e6de7a3071fe437b663217e28c8d8
setodeve <keinick11@outlook.com>
2025-03-05 22:37:46 +0900

    fix: make resume title bold in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className="inset-x-0 bg-neutral-300 dark:bg-neutral-900 sm:relative">
       <div className="mx-auto justify-between p-3 sm:flex sm:justify-between sm:max-w-4xl sm:p-4">
-        <div className="text-2xl">Resume {cat}</div>
+        <div className="text-2xl font-bold">Resume {cat}</div>
         <div className="flex items-center font-bold justify-end">
           <Link className="mx-2" href="https://github.com/setodeve" target="_blank" aria-label="GitHub">Github</Link>
           <Link className="mx-2" href="https://qiita.com/keiswe" target="_blank" aria-label="Qiita">Qiita</Link>
